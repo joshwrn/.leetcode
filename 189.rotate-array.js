6a48416e16b8885f030932ac44890601346cd52e
joshwrn <joshnwarren@gmail.com>
2022-03-04 23:51:31 -0800

    189

--- 189.rotate-array.js
+++ 189.rotate-array.js
@@ -16,9 +16,21 @@ Output: [5,6,7,1,2,3,4]
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // reverse all then split at k and reverse both sides
-var rotate = function (arr, k) {
-  const n = arr.length - 1;
-  let left = 0;
-  let right = k;
+var rotate = function (arr, kth) {
+  n = arr.length - 1;
+  const k = kth % (n + 1);
+  reverse(arr, 0, n);
+  reverse(arr, 0, k - 1);
+  reverse(arr, k, n);
+  return arr;
 };
+
+const reverse = (arr, left, right) => {
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+};
+
 // @lc code=end
