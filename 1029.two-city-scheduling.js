8d5a8374044e80aee69ab907bbaa2fdd1744894d
joshwrn <joshnwarren@gmail.com>
2022-03-26 01:30:36 -0700

    1029 faster

--- 1029.two-city-scheduling.js
+++ 1029.two-city-scheduling.js
@@ -10,13 +10,16 @@
  * @return {number}
  */
 var twoCitySchedCost = function (costs) {
-  const diff = costs.sort((a, b) => a[0] - a[1] - (b[0] - b[1]));
+  costs.sort((a, b) => a[0] - a[1] - (b[0] - b[1]));
 
   let res = 0;
 
-  for (let i = 0; i < diff.length; i++) {
-    if (i < diff.length / 2) res += diff[i][0];
-    if (i >= diff.length / 2) res += diff[i][1];
+  for (let i = 0; i < costs.length; i++) {
+    if (i < costs.length / 2) {
+      res += costs[i][0];
+    } else {
+      res += costs[i][1];
+    }
   }
 
   return res;
